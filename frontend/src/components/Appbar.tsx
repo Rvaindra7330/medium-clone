a7464b175e494530a1599faa17d65bfeada5ba08
Ravindra7330 <ravindarravi87@gmail.com>
2024-12-27 23:57:25 +0530

    avatar size changes

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,11 +1,11 @@
 import { Avatar } from "./BlogCard"
 export const Appbar=()=>{
     return <div className="border-b flex justify-between px-10 py-4">
-        <div>
+        <div className="flex flex-col justify-center">
             Medium
         </div>
         <div>
-            <Avatar name="Ravindra"></Avatar>
+            <Avatar size={"big"} name="Ravindra"></Avatar>
         </div>
 
     </div>
--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -38,8 +38,8 @@ function Circle(){
 
     </div>
 }
- export function Avatar({ name}:{ name:string}){
-    return <div className="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-    <span className="font-extrlight text-gray-600 dark:text-gray-300">{name[0].toUpperCase()}</span>
+ export function Avatar({ name,size="small"}:{ name:string,size:"small"| "big"}){
+    return <div className={`relative inline-flex items-center justify-center ${size==="small"?"w-6 h-6":"w-10 h-10"} overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600`}>
+    <span className={`${size=="small"?"text-xs":"text-md"} font-extrlight text-gray-600 dark:text-gray-300 `}>{name[0].toUpperCase()}</span>
 </div>
 }
